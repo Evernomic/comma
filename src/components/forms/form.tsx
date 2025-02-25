96082db1a36305d71d144911dd5e8ded7b496995
Manafov <mehmanmanafov@icloud.com>
2025-02-25 17:28:50 +0400

    add toggle branding

--- src/components/forms/form.tsx
+++ src/components/forms/form.tsx
@@ -7,8 +7,10 @@ import { type FormEvent, useMemo, useState, useTransition } from "react";
 import { Icons } from "../shared/icons";
 import Button from "../ui/button";
 import Input from "../ui/input";
+import { Switch } from "../ui/switch";
 import { Textarea } from "../ui/textarea";
 import { toast } from "../ui/use-toast";
+import Upgrade from "../shared/upgrade";
 
 interface FormProps {
   title: string;
@@ -22,8 +24,11 @@ interface FormProps {
   required?: boolean;
   prefix?: string;
   suffix?: string;
+
   asChild?: boolean;
+  toggle?: boolean;
   children?: React.ReactNode;
+  proFeature?: boolean
 }
 
 export default function Form({
@@ -40,11 +45,13 @@ export default function Form({
   suffix,
   required = true,
   asChild = false,
+  toggle = false,
+  proFeature = false,
 }: FormProps) {
   const [saving, startTransition] = useTransition();
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const [value, setValue] = useState(
+  const [value, setValue] = useState<any>(
     inputData?.defaultValue || textareaData?.defaultValue || "",
   );
   const router = useRouter();
@@ -102,55 +109,71 @@ export default function Form({
 
   return (
     <form
-      className="overflow-hidden rounded-md border border-gray-2"
+      className="overflow-hidden relative rounded-md border border-gray-2"
       onSubmit={onSubmit}
     >
+      {proFeature && <Upgrade />}
       <div className="flex flex-col gap-1 p-4">
         <h1>{title}</h1>
         <p className="text-sm text-gray-4">{description}</p>
-        {!asChild && (
+        {!asChild ? (
           <div className="mt-2">
             {type === "input" ? (
-              <div className="flex items-center relative w-max">
-                {prefix && (
-                  <span className="h-5 rounded-l-md bg-gray-3 flex items-center justify-center px-2 border border-gray-2 border-r-0 text-sm text-gray-4">
-                    {prefix}
-                  </span>
-                )}
-                <Input
-                  {...inputData}
-                  type={
-                    inputData?.type === "password" && showPassword
-                      ? "text"
-                      : (inputData?.type ?? "text")
-                  }
-                  value={value}
-                  autoComplete="off"
-                  className={cn(
-                    "w-[250px] max-md:w-full",
-                    prefix ? "rounded-l-none " : "",
-                    suffix ? "rounded-r-none" : "",
-                  )}
-                  onChange={(e) => setValue(e.target.value)}
-                />
-                {inputData?.type === "password" && (
-                  <span
-                    className="text-gray-4 cursor-pointer absolute right-3"
-                    onClick={() => setShowPassword((prev) => !prev)}
-                  >
-                    {showPassword ? (
-                      <Icons.eye size={18} />
-                    ) : (
-                      <Icons.eyeOff size={18} />
+              <>
+                {toggle ? (
+                  <div className="flex items-center gap-2 text-sm text-gray-4">
+                    <Switch
+                      type="submit"
+                      onCheckedChange={(checked) => setValue(checked)}
+                      defaultChecked={inputData?.defaultChecked}
+                    />{" "}
+                    <label>
+                      {inputData?.defaultChecked ? "Enabled" : "Disabled"}
+                    </label>
+                  </div>
+                ) : (
+                  <div className="flex items-center relative w-max">
+                    {prefix && (
+                      <span className="h-5 rounded-l-md bg-gray-3 flex items-center justify-center px-2 border border-gray-2 border-r-0 text-sm text-gray-4">
+                        {prefix}
+                      </span>
                     )}
-                  </span>
+                    <Input
+                      {...inputData}
+                      type={
+                        inputData?.type === "password" && showPassword
+                          ? "text"
+                          : (inputData?.type ?? "text")
+                      }
+                      value={value}
+                      autoComplete="off"
+                      className={cn(
+                        "w-[250px] max-md:w-full",
+                        prefix ? "rounded-l-none " : "",
+                        suffix ? "rounded-r-none" : "",
+                      )}
+                      onChange={(e) => setValue(e.target.value)}
+                    />
+                    {inputData?.type === "password" && (
+                      <span
+                        className="text-gray-4 cursor-pointer absolute right-3"
+                        onClick={() => setShowPassword((prev) => !prev)}
+                      >
+                        {showPassword ? (
+                          <Icons.eye size={18} />
+                        ) : (
+                          <Icons.eyeOff size={18} />
+                        )}
+                      </span>
+                    )}
+                    {suffix && (
+                      <span className="h-5 rounded-r-md bg-gray-3 flex items-center justify-center px-2 border border-gray-2 border-l-0 text-sm text-gray-4">
+                        {suffix}
+                      </span>
+                    )}
+                  </div>
                 )}
-                {suffix && (
-                  <span className="h-5 rounded-r-md bg-gray-3 flex items-center justify-center px-2 border border-gray-2 border-l-0 text-sm text-gray-4">
-                    {suffix}
-                  </span>
-                )}
-              </div>
+              </>
             ) : (
               <Textarea
                 className="w-[350px] max-md:w-full"
@@ -162,18 +185,23 @@ export default function Form({
               />
             )}
           </div>
+        ) : (
+          children
         )}
-        {asChild && children}
       </div>
       <footer className="flex h-auto flex-row items-center   justify-between border-t border-gray-2 bg-gray-3 px-4 py-2">
         <div className={cn("text-sm text-gray-4", error ? "text-danger" : "")}>
-          {error || helpText}
+          {saving ? (
+            <Icons.spinner className="animate-spin" size={15} />
+          ) : (
+            error || helpText
+          )}
         </div>
 
         <Button
-          type="submit"
+          type={toggle || asChild ? "button" : "submit"}
           size="sm"
-          className={cn(asChild && "invisible")}
+          className={cn((toggle || asChild) && "invisible")}
           disabled={disabledButton}
           isPending={saving}
         >
