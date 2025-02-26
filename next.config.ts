08ccae4cbafcc95c04c48f31a7bafadd0baae2b5
Manafov <mehmanmanafov@icloud.com>
2025-02-26 21:23:42 +0400

    fix redirects

--- next.config.ts
+++ next.config.ts
@@ -11,27 +11,11 @@ const nextConfig: NextConfig = {
             type: "host",
             value: "go.comma.to",
           },
-          {
-            type: "host",
-            value: "go.nucelo.co"
-          }
         ],
         destination: `${process.env.NEXT_PUBLIC_URL}/api/bookmarks/t/:id`,
         permanent: false,
-      },
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: `app.${process.env.NEXT_PUBLIC_LEGACY_APP_DOMAIN}`,
-          }
-        ],
-        destination: `${process.env.NEXT_PUBLIC_APP_URL}/:path*`,
-        permanent: true,
       }
     ];
-
   },
   images: {
     remotePatterns: [
