13de0f5d1bfcd105268f04824cd7489106b8a1e8
Manafov <mehmanmanafov@icloud.com>
2025-03-29 20:05:21 +0400

    Update nav-tabs.tsx

--- src/app/(user)/user/[domain]/components/nav-tabs.tsx
+++ src/app/(user)/user/[domain]/components/nav-tabs.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ClientOnly from "@/components/shared/client-only";
 import { cn, sortUserNavItems } from "@/lib/utils";
 import type { CustomNavItem, NavItem, User } from "@/types";
 import Link from "next/link";
@@ -11,28 +12,32 @@ export default function NavTabs({
   user: User;
   pages: CustomNavItem[];
 }) {
-  const pathname = usePathname();
+  const pathname = usePathname()
   const links = sortUserNavItems(
     pages,
     user.externalLinks as NavItem[],
   )?.filter((l) => (l.isVisible !== undefined ? l.isVisible : true));
+
   if (!links.length) {
     return null;
   }
   return (
-    <nav className="flex gap-4 mb-6">
-      {links.map((page) => (
-        <Link
-          href={page.href}
-          key={page.href}
-          className={cn(
-            "text-sm relative transition-colors text-gray-4 before:multi-[content-[''];h-[1.5px];w-full;bg-transparent;absolute;-bottom-[4.5px];transition-colors] hover:text-secondary",
-            pathname === page.href && "text-secondary before:bg-secondary",
-          )}
-        >
-          {page.title}
-        </Link>
-      ))}
-    </nav>
+    <ClientOnly>
+      <nav className="flex gap-4 mb-6">
+        {links.map((page) => (
+          <Link
+            href={page.href}
+            key={page.href}
+            className={cn(
+              "text-sm relative transition-colors text-gray-4 before:multi-[content-[''];h-[1.5px];w-full;bg-transparent;absolute;-bottom-[4.5px];transition-colors] hover:text-secondary",
+              pathname === page.href && "text-secondary before:bg-secondary",
+            )}
+          >
+            {page.title}
+          </Link>
+        ))}
+      </nav>
+    </ClientOnly>
+
   );
 }
