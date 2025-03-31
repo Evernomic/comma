656e77f04a525e457a233e5d9cb97ca342d8f7c9
Manafov <mehmanmanafov@icloud.com>
2025-03-31 18:10:09 +0400

    Minor IX

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,25 +4,27 @@ import type React from "react";
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   title?: string;
   description?: string;
+  titleAsChild?: boolean;
 }
 
 export default function AppHeader({
   title,
   description,
   children,
+  titleAsChild,
   className,
 }: Props) {
   return (
     <div className={cn("flex items-center justify-between", className)}>
       {(title || description) && (
         <div className="flex flex-col gap-1">
-          {title && <h3 className="title text-lg font-medium ">{title}</h3>}
+          {titleAsChild ?  children : title && <h3 className="title text-lg font-medium ">{title}</h3>}
           {description && (
             <p className="description text-sm text-gray-4">{description}</p>
           )}
         </div>
       )}
-      {children}
+      {!titleAsChild && children}
     </div>
   );
 }
