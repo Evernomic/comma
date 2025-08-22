f5cfc96a1b1d68fde71cba15738fbd95f778b5fa
Manafov <mehmanmanafov@icloud.com>
2025-08-23 03:41:08 +0400

    fix viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,10 @@ export const viewport: Viewport = {
       color: "#0d0d0d",
     },
   ],
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
